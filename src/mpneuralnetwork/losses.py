fix: Fixed CrossEntropy for Softmax

src/mpneuralnetwork/losses.py
@@ -24,7 +24,7 @@ class MSE(Loss):
 class CrossEntropy(Loss):
     def direct(self, output, output_expected):
         epsilon = 1e-9
-        return np.mean(-output_expected * np.log(output + epsilon))
+        return -np.sum(output_expected * np.log(output + epsilon))
 
     def prime(self, output, output_expected):
-        return (output_expected - output) / output.size
+        return output_expected - output