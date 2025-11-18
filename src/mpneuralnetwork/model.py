fix: Auto execute a forward from a Softmax in the predict if the loss is CCE. This is a workaround, waiting for the next implementation of the model class.

src/mpneuralnetwork/model.py
@@ -1,5 +1,7 @@
 import numpy as np
-from .losses import MSE
+
+from .activations import Softmax
+from .losses import MSE, CategoricalCrossEntropy
 from .optimizers import SGD
 
 
@@ -85,5 +87,8 @@ class Model:
         output = np.copy(input)
         for layer in self.layers:
             output = layer.forward(output)
+        
+        if isinstance(self.loss, CategoricalCrossEntropy):
+            output = Softmax().forward(output)
 
         return output