61f064038b12e913cdfaaf89dba9d2ceee574419
Maxime Pires <maximepires4@gmail.com>
2025-11-18 18:28:38 +0100

    chore: Deleted old utils file

--- src/mpneuralnetwork/utils.py
+++
@@ -1,8 +0,0 @@
-import random
-
-
-def shuffle(input, output):
-    combined = list(zip(input, output))
-    random.shuffle(combined)
-    input[:], output[:] = zip(*combined)
-    return input, output
